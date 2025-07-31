aa02464ff3daa63d9f9013aa9e866d873a13ee52
Graham Seacat <86634457+gseacat@users.noreply.github.com>
2025-07-31 15:41:28 -0500

    Update scrape_projections.R

--- scrape_projections.R
+++ scrape_projections.R
@@ -89,3 +89,7 @@ df <- combined_df %>%
   arrange(desc(half_ppr_points)) %>% 
   # Select only the necessary columns for GitHub project
   select(player, team, pos, standard_points, half_ppr_points, ppr_points)
+
+
+# Write the projections data to the data folder
+write_csv(df, "data/projections_2025_ffanalytics.csv")
